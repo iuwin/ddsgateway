22902fe10371b43128f51ab70383194cea756687
Erwin Amper <40266802+iuwin@users.noreply.github.com>
2021-06-10 23:02:44 +0800

    api gateway update

---
+++ app/Services/User2Service.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Services;
+
+use App\Traits\ConsumesExternalService;
+
+class User2Service{
+
+    use ConsumesExternalService;
+
+    /**
+     * The base uri to consume the User2 Service
+     * @var string
+     */
+    public $baseUri;
+
+    public function __construct()
+    {
+        $this->baseUri = config('services.users2.base_uri');
+    }
+
+
+    public function createUser2($data)
+    {
+        return $this->performRequest('POST', '/users', $data);
+    }
+
+
+    public function obtainUsers2()
+    {
+        return $this->performRequest('GET','/users');
+    }
+
+
+    public function editUser2($data, $id)
+    {
+       return $this->performRequest('PUT', "/users/{$id}", $data);
+    }
+
+    public function deleteUser2($id)
+    {
+        return $this->performRequest('DELETE', "/users/{$id}");
+    }
+}

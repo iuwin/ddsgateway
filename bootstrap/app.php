e6d0236afebed16a9ac2b23c842777c7e8eb3f47
Erwin Amper <40266802+iuwin@users.noreply.github.com>
2021-06-14 12:48:50 +0800

    deployment to heroku updates

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -29,6 +29,8 @@ $app->withEloquent();
 
 $app->configure('services');
 $app->configure('auth');
+$app->configure('database');
+
 
 /*
 |--------------------------------------------------------------------------
